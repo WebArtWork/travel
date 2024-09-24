1531a589879cbccdf3be0bea820d3d53be407882
lizkyi <andriilizvinskyi@gmail.com>
2024-09-24 18:22:45 +0300

    merge 1

--- js/index.js
+++ js/index.js
@@ -157,4 +157,31 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
-  
+  $('.feedback-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.drive-slider').slick({
+    arrows: false,
+    dots: true
+  });
